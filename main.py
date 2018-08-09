45c9c2ac29aa7fe7ad50b97c34fc0ccb08a45836
hamish <hamish.self@gmail.com>
2018-08-09 13:34:11 +1000

    Staging my sim

--- main.py
+++ main.py
@@ -1,31 +1,53 @@
-# External packages
-import numpy as np                  # Maths 
-import scipy                        # Science
-import matplotlib.pyplot as plt     # MATLAB-style plotting
-# Internal modules
-import Modules.Drag as Drag         # the "as" syntax lets you use an alias (no need to write Modules. every time!)
-import Modules.Gravity as Gravity 
-import Modules.Thrust as Thrust 
-import Modules.Rocket as Rocket
-import Modules.Integrator as Integrator
+__author__ = 'Hamish Self'
 
-## Initialisation
+import Code.sim_code as s
+import matplotlib.pyplot as plt
 
-
-
-## Main Loop
+# Rocket Data
+t_0 = 0                             # time after launch to begin simulation, s
+m_total_0 = 25                      # kg
+m_fuel_0 = 9                        # kg
+a_surf = 0.07712                    # surface area (cylindrical approx), m^2
+T_0 = 3000                          # peak thrust, N
+t_B = 4.5                           # burnout time, secs
+alt_0 = 0                           # initial altitude, m
+vel_0 = 0                           # initial velocity, m s^-1
 
 
+# Initialise pre-launch state
+# State(m_total, m_fuel, T_0, t_B, altitude, velocity, acceleration, time)
+initial_state = s.State(m_total_0, m_fuel_0, a_surf, T_0, t_B, alt_0, vel_0, t_0)
+print('Initial state: {}'.format(initial_state.summary()))
 
-## Plotting
-# When you have some data ready to plot, uncomment the code below (ctrl+/)
-# plt.figure(1)
-# plt.subplot(211)
-# plt.plot(time, height)
-# plt.xlabel('Time [s]')
-# plt.ylabel('Altitude [m]')
-# plt.subplot(211)
-# plt.plot(time, velocity)
-# plt.xlabel('Time [s]')
-# plt.ylabel('Velocity [m/s]')
-# plt.show()
+state_array, sim_time = s.run_simulation(initial_state, dt=0.003)
+print('Simulation time: {}s'.format(sim_time))
+
+# Post-processing steps
+alt_array = [s.alt for s in state_array]
+time_array = [s.time for s in state_array]
+vel_array = [s.vel for s in state_array]
+accel_array = [s.acc for s in state_array]
+
+plt.figure()
+plt.subplot(2, 2, 1)
+plt.plot(time_array,alt_array,'r-')
+plt.xlabel('time, seconds')
+plt.ylabel('z(t), m')
+plt.grid()
+
+plt.subplot(2, 2, 2)
+plt.plot(time_array,vel_array,'r-')
+plt.xlabel('time, seconds')
+plt.ylabel('v(t), m s^-1')
+plt.grid()
+
+plt.subplot(2, 2, 3)
+plt.plot(time_array,accel_array,'r-')
+plt.xlabel('time, seconds')
+plt.ylabel('a(t), m s^-2')
+plt.grid()
+
+plt.show()
+
+
+# ALSO NEED TO ACCOUNT FOR PARACHUTE DEPLOYMENT DRAG - ASK OLI
